da39046cd83d114d8ddb1089572e1ae0c4171e9f
Rayan Raddatz <rayandematos@gmail.com>
2025-07-03 22:23:49 -0300

    Cleaned a little bit the code and create separeted files for the confings (globas and includes) and lab functions.

---
+++ include/configs.h
@@ -0,0 +1,177 @@
+#ifndef _CONFIGS_H
+#define _CONFIGS_H
+
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+// Headers abaixo são específicos de C++
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <limits>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
+
+// Headers das bibliotecas OpenGL
+#include <glad/glad.h>   // Criação de contexto OpenGL 3.3
+#include <GLFW/glfw3.h>  // Criação de janelas do sistema operacional
+
+// Headers da biblioteca GLM: criação de matrizes e vetores.
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Headers da biblioteca para carregar modelos obj
+#include <tiny_obj_loader.h>
+
+#include <stb_image.h>
+
+// Headers locais, definidos na pasta "include/"
+#include "utils.h"
+#include "matrices.h"
+
+// Structs utilizadas
+
+struct SceneObject
+{
+    std::string  name;
+    size_t       first_index;
+    size_t       num_indices;
+    GLenum       rendering_mode;
+    GLuint       vertex_array_object_id;
+    glm::vec3    bbox_min;
+    glm::vec3    bbox_max;
+};
+
+
+struct ObjModel
+{
+    tinyobj::attrib_t                 attrib;
+    std::vector<tinyobj::shape_t>     shapes;
+    std::vector<tinyobj::material_t>  materials;
+
+    ObjModel(const char* filename, const char* basepath = NULL, bool triangulate = true)
+    {
+        printf("Carregando objetos do arquivo \"%s\"...\n", filename);
+
+        std::string fullpath(filename);
+        std::string dirname;
+        if (basepath == NULL)
+        {
+            auto i = fullpath.find_last_of("/");
+            if (i != std::string::npos)
+            {
+                dirname = fullpath.substr(0, i+1);
+                basepath = dirname.c_str();
+            }
+        }
+
+        std::string warn;
+        std::string err;
+        bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename, basepath, triangulate);
+
+        if (!err.empty())
+            fprintf(stderr, "\n%s\n", err.c_str());
+
+        if (!ret)
+            throw std::runtime_error("Erro ao carregar modelo.");
+
+        for (size_t shape = 0; shape < shapes.size(); ++shape)
+        {
+            if (shapes[shape].name.empty())
+            {
+                fprintf(stderr,
+                        "*********************************************\n"
+                        "Erro: Objeto sem nome dentro do arquivo '%s'.\n"
+                        "Veja https: link sobre algo aqui"
+                        "*********************************************\n",
+                    filename);
+                throw std::runtime_error("Objeto sem nome.");
+            }
+            printf("- Objeto '%s'\n", shapes[shape].name.c_str());
+        }
+
+        printf("OK.\n");
+    }
+};
+
+// Abaixo definimos variáveis globais utilizadas em várias funções do código.
+
+// A cena virtual é uma lista de objetos nomeados, guardados em um dicionário
+// (map).  Veja dentro da função BuildTrianglesAndAddToVirtualScene() como que são incluídos
+// objetos dentro da variável g_VirtualScene, e veja na função main() como
+// estes são acessados.
+std::map<std::string, SceneObject> g_VirtualScene;
+
+// Pilha que guardará as matrizes de modelagem.
+std::stack<glm::mat4>  g_MatrixStack;
+
+// Razão de proporção da janela (largura/altura). Veja função FramebufferSizeCallback().
+float g_ScreenRatio = 1.0f;
+
+// Ângulos de Euler que controlam a rotação de um dos cubos da cena virtual
+float g_AngleX = 0.0f;
+float g_AngleY = 0.0f;
+float g_AngleZ = 0.0f;
+
+// "g_LeftMouseButtonPressed = true" se o usuário está com o botão esquerdo do mouse
+// pressionado no momento atual. Veja função MouseButtonCallback().
+bool g_LeftMouseButtonPressed = false;
+bool g_RightMouseButtonPressed = false; // Análogo para botão direito do mouse
+bool g_MiddleMouseButtonPressed = false; // Análogo para botão do meio do mouse
+
+// "g_KeyW_Pressed = true" se o usuário está com a tecla W do teclado pressionada
+// no momento atual. Veja função KeyCallback();
+bool g_KeyW_Pressed = false;
+
+// "g_KeyW_Pressed = true" se o usuário está com a tecla S do teclado pressionada
+// no momento atual. Veja função KeyCallback();
+bool g_KeyS_Pressed = false;
+
+// "g_KeyW_Pressed = true" se o usuário está com a tecla D do teclado pressionada
+// no momento atual. Veja função KeyCallback();
+bool g_KeyD_Pressed = false;
+
+// "g_KeyW_Pressed = true" se o usuário está com a tecla A do teclado pressionada
+// no momento atual. Veja função KeyCallback();
+bool g_KeyA_Pressed = false;
+
+// Variáveis que definem a câmera em coordenadas esféricas, controladas pelo
+// usuário através do mouse (veja função CursorPosCallback()). A posição
+// efetiva da câmera é calculada dentro da função main(), dentro do loop de
+// renderização.
+float g_CameraTheta = 3.141592f; // Ângulo no plano ZX em relação ao eixo Z
+float g_CameraPhi = 0.0f;   // Ângulo em relação ao eixo Y
+float g_CameraDistance = 3.5f; // Distância da câmera para a origem
+
+// Variáveis que controlam rotação do antebraço
+float g_ForearmAngleZ = 0.0f;
+float g_ForearmAngleX = 0.0f;
+
+// Variáveis que controlam translação do torso
+float g_TorsoPositionX = 0.0f;
+float g_TorsoPositionY = 0.0f;
+
+// Variável que controla o tipo de projeção utilizada: perspectiva ou ortográfica.
+bool g_UsePerspectiveProjection = true;
+
+// Variável que controla se o texto informativo será mostrado na tela.
+bool g_ShowInfoText = true;
+
+// Variáveis que definem um programa de GPU (shaders). Veja função LoadShadersFromFiles().
+GLuint g_GpuProgramID = 0;
+GLint g_model_uniform;
+GLint g_view_uniform;
+GLint g_projection_uniform;
+GLint g_object_id_uniform;
+GLint g_bbox_min_uniform;
+GLint g_bbox_max_uniform;
+
+// Número de texturas carregadas pela função LoadTextureImage()
+GLuint g_NumLoadedTextures = 0;
+
+#endif
