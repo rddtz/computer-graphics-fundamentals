8323018dc106203bf9f1161945870c2aa88e1de9
Rayan Raddatz <rayandematos@gmail.com>
2025-07-06 10:10:59 -0300

    now checking collision with the walls

---
+++ include/collision.h
@@ -0,0 +1,166 @@
+#ifndef _COLLISIONS_H
+#define _COLLISIONS_H
+
+#include "configs.h"
+
+
+int CheckColisionPointToPlane(glm::vec4 point, glm::vec4 pn, glm::vec4 point_in_plane);
+int CheckColisionAABBtoAABB(SceneObject obj1, glm::mat4 transf1, SceneObject obj2, glm::mat4 transf2);
+int CheckColisionAABBtoPlane(BoundingBox box, glm::vec4 pn, float pd);
+int CheckColisionPointWalls(glm::vec4 point);
+
+BoundingBox GetBboxModel(ObjModel* model, glm::mat4 transformation);
+
+
+int CheckColisionPointToPlane(glm::vec4 point, glm::vec4 pn, glm::vec4 point_in_plane){
+
+  //                    moving the point to not enter in the wall
+  glm::vec4 v = point - (point_in_plane + 0.001f*pn);
+
+  float d = dotproduct(pn, v);
+
+  if(d > 0.01f){
+    return 0;
+  }
+
+  return 1;
+
+}
+
+int CheckColisionAABBtoAABB(SceneObject obj1, glm::mat4 transf1, SceneObject obj2, glm::mat4 transf2){
+
+  BoundingBox obj1b = {transf1 * glm::vec4(obj1.bbox_min.x, obj1.bbox_min.y, obj1.bbox_min.z, 1), transf1 * glm::vec4(obj1.bbox_max.x, obj1.bbox_max.y, obj1.bbox_max.z, 1)};
+  BoundingBox obj2b = {transf2 * glm::vec4(obj2.bbox_min.x, obj2.bbox_min.y, obj2.bbox_min.z, 2), transf2 * glm::vec4(obj2.bbox_max.x, obj2.bbox_max.y, obj2.bbox_max.z, 2)};
+
+  int collisionX = obj1b.max.x >= obj2b.min.x && obj2b.max.x >= obj1b.min.x;
+
+  int collisionY = obj1b.max.y >= obj2b.min.y && obj2b.max.y >= obj1b.min.y;
+
+  int collisionZ = obj1b.max.z >= obj2b.min.z && obj2b.max.z >= obj1b.min.z;
+
+  return collisionX && collisionY && collisionZ;
+
+}
+
+
+int CheckColisionAABBtoPlane(BoundingBox box, glm::vec4 pn, float pd){
+
+  glm::vec4 center = (box.max + box.min)/2.0f;
+  glm::vec4 e = box.max - center;
+
+  float r = e.x * abs(pn.x) + e.y * abs(pn.y) + e.z * abs(pn.z);
+
+  float s = dotproduct(pn, center) - pd;
+
+  return abs(s) <= r;
+
+}
+
+
+int CheckColisionPointWalls(glm::vec4 point){
+
+  BoundingBox wallPoints = {glm::vec4(g_VirtualScene["the_plane"].bbox_min.x, g_VirtualScene["the_plane"].bbox_min.y, g_VirtualScene["the_plane"].bbox_min.z, 1),
+			    glm::vec4(g_VirtualScene["the_plane"].bbox_max.x, g_VirtualScene["the_plane"].bbox_max.y, g_VirtualScene["the_plane"].bbox_max.z, 1)};
+
+  glm::mat4 wallDefault = Matrix_Rotate_X(3.141592f /2) * Matrix_Scale(10.0f, 1.0f, 10.0f);
+
+
+  // Testing for the first wall
+  glm::mat4 wallModel = Matrix_Translate(0.0f,3.9f,-10.0f) * wallDefault;
+  BoundingBox wall = {wallModel * wallPoints.min, wallModel * wallPoints.max};
+
+  glm::vec4 wall_normal = crossproduct(glm::vec4(wall.min.x, wall.max.y, wall.max.z, 1) - wall.max,
+				       glm::vec4(wall.min.x, wall.max.y, wall.max.z, 1) - wall.min);
+
+  int c1 = CheckColisionPointToPlane(point, wall_normal, wall.min);
+
+
+  // Testing for the second wall
+  wallModel = Matrix_Translate(-10.0f,3.9f,0.0f) * Matrix_Rotate_Y(3.141592f/2) * wallDefault;
+
+  wall = {wallModel * wallPoints.min, wallModel * wallPoints.max};
+  wall_normal = crossproduct(glm::vec4(wall.max.x, wall.max.y, wall.min.z, 1) - wall.max,
+			     glm::vec4(wall.max.x, wall.max.y, wall.min.z, 1) - wall.min);
+
+  int c2 = CheckColisionPointToPlane(point, wall_normal, wall.min);
+
+
+  // Testing for the third wall
+  wallModel = Matrix_Translate(10.0f,3.9f,0.0f) * Matrix_Rotate_Y(-3.141592f/2) * wallDefault;
+
+  wall = {wallModel * wallPoints.min, wallModel * wallPoints.max};
+  wall_normal = crossproduct(glm::vec4(wall.max.x, wall.max.y, wall.min.z, 1) - wall.max,
+			     glm::vec4(wall.max.x, wall.max.y, wall.min.z, 1) - wall.min);
+
+  int c3 = CheckColisionPointToPlane(point, wall_normal, wall.min);
+
+  // Testing for the forth wall
+
+  wallModel = Matrix_Translate(0.0f,3.9f,10.0f) * Matrix_Rotate_Y(3.141592f) * wallDefault;
+
+  wall = {wallModel * wallPoints.min, wallModel * wallPoints.max};
+  wall_normal = crossproduct(glm::vec4(wall.min.x, wall.max.y, wall.max.z, 1) - wall.max,
+			     glm::vec4(wall.min.x, wall.max.y, wall.max.z, 1) - wall.min);
+
+  int c4 = CheckColisionPointToPlane(point, wall_normal, wall.min);
+
+  //if at leat one collision, the player cant move
+  return c1 || c2 || c3 || c4;
+
+}
+
+
+/*Get the perfect bounding box for the object at the moment */
+BoundingBox GetBboxModel(ObjModel* model, glm::mat4 transformation){
+
+   GLuint vertex_array_object_id;
+    glGenVertexArrays(1, &vertex_array_object_id);
+    glBindVertexArray(vertex_array_object_id);
+
+    std::vector<GLuint> indices;
+    std::vector<float>  model_coefficients;
+    std::vector<float>  normal_coefficients;
+    std::vector<float>  texture_coefficients;
+
+  size_t first_index = indices.size();
+  size_t num_triangles = model->shapes[0].mesh.num_face_vertices.size();
+
+  const float minval = std::numeric_limits<float>::min();
+  const float maxval = std::numeric_limits<float>::max();
+
+  glm::vec4 bbox_min = glm::vec4(maxval,maxval,maxval, 1);
+  glm::vec4 bbox_max = glm::vec4(minval,minval,minval, 1);
+
+  for (size_t triangle = 0; triangle < num_triangles; ++triangle)
+    {
+      assert(model->shapes[0].mesh.num_face_vertices[triangle] == 3);
+
+      for (size_t vertex = 0; vertex < 3; ++vertex)
+	{
+	  tinyobj::index_t idx = model->shapes[0].mesh.indices[3*triangle + vertex];
+
+	  indices.push_back(first_index + 3*triangle + vertex);
+
+	  glm::vec4 v = transformation * glm::vec4(model->attrib.vertices[3*idx.vertex_index + 0],
+						   model->attrib.vertices[3*idx.vertex_index + 1],
+						   model->attrib.vertices[3*idx.vertex_index + 2],
+						   1);
+
+
+	  bbox_min.x = std::min(bbox_min.x, v.x);
+	  bbox_min.y = std::min(bbox_min.y, v.y);
+	  bbox_min.z = std::min(bbox_min.z, v.z);
+	  bbox_max.x = std::max(bbox_max.x, v.x);
+	  bbox_max.y = std::max(bbox_max.y, v.y);
+	  bbox_max.z = std::max(bbox_max.z, v.z);
+
+
+	}
+    }
+
+  BoundingBox ret = {bbox_min, bbox_max};
+  return ret;
+
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include "configs.h" // Globals variables, includes and structs
 #include "lab_functions.h" // Functions defined in the labs
+#include "collision.h" // Collision functions
 
 void LoadTextures(); // Function to Load the texture from images
 void DrawObject(glm::mat4 model, const char* name, int id); // Function to draw the object in the screen
+void PrintVec4(glm::vec4 vec);
 
 #define SPHERE 0
 #define BUNNY  1
 #define PLANE  2
 #define PORTALGUN 3
+#define CUBE 4
 
 int main(int argc, char* argv[])
 {
@@ -75,6 +78,12 @@ int main(int argc, char* argv[])
   ComputeNormals(&planemodel);
   BuildTrianglesAndAddToVirtualScene(&planemodel);
 
+
+  ObjModel cube("../../data/the_cube.obj");
+  ComputeNormals(&cube);
+  BuildTrianglesAndAddToVirtualScene(&cube);
+
+
   ObjModel gunmodel("../../textures/portalgun/portalgun.obj");
   BuildTrianglesAndAddToVirtualScene(&gunmodel);
 
@@ -84,6 +93,8 @@ int main(int argc, char* argv[])
       BuildTrianglesAndAddToVirtualScene(&model);
     }
 
+  // GLuint bounding_box_object_id = CreateBox();
+
   TextRendering_Init();
 
   glEnable(GL_DEPTH_TEST);
@@ -92,9 +103,32 @@ int main(int argc, char* argv[])
   glCullFace(GL_BACK);
   glFrontFace(GL_CCW);
 
-  glm::vec4 camera_position_c  = glm::vec4(0.0f,4.0f,5.0f,1.0f);
+  glm::vec4 camera_position_c  = glm::vec4(0.0f,0.0f,5.0f,1.0f);
   float speed = 3.0f;
   float prev_time = (float) glfwGetTime();
+
+  PrintObjModelInfo(&planemodel);
+  for (auto const& x : g_VirtualScene)
+    {
+      std::cout << x.first  // string (key)
+		<< ':'
+		<< x.second.name // string's value
+		<< ":Min(" << x.second.bbox_min.x
+		<< ", " << x.second.bbox_min.y
+	        << ", " << x.second.bbox_min.y
+		<< ")"
+		<< ":Max(" << x.second.bbox_max.x
+		<< ", " << x.second.bbox_max.y
+	        << ", " << x.second.bbox_max.y
+		<< ")"
+		<< ":FirstIndex(" << x.second.first_index
+		<< ")"
+		<< ":NumIndices(" << x.second.num_indices
+		<< ")"
+		<< ":VAO_ID(" << x.second.vertex_array_object_id
+		<< ")"
+		<< std::endl;
+    }
 
   while (!glfwWindowShouldClose(window))
     {
@@ -120,19 +154,41 @@ int main(int argc, char* argv[])
       glm::vec4 camera_w_vector = -(camera_view_vector / norm(camera_view_vector));
       camera_w_vector.y = 0;
 
+      glm::vec4 cam_temp = camera_position_c;
+
       if(g_KeyW_Pressed){
-	camera_position_c += -camera_w_vector * speed * delta_t;
+
+	cam_temp += -camera_w_vector * speed * delta_t;
+
+	if(!CheckColisionPointWalls(cam_temp)){
+	  camera_position_c = cam_temp;
+	}
+
       }
       if(g_KeyS_Pressed){
-	camera_position_c += camera_w_vector * speed * delta_t;
+	cam_temp += camera_w_vector * speed * delta_t;
+
+	if(!CheckColisionPointWalls(cam_temp)){
+	  camera_position_c = cam_temp;
+	}
       }
-      if(g_KeyD_Pressed){
+
+      if(g_KeyD_Pressed ){
 	glm::vec4 upw_crossprod = crossproduct(camera_up_vector, camera_w_vector);
-	camera_position_c += (upw_crossprod / norm(upw_crossprod)) * speed * delta_t;
+	cam_temp += (upw_crossprod / norm(upw_crossprod)) * speed * delta_t;
+
+	if(!CheckColisionPointWalls(cam_temp)){
+	  camera_position_c = cam_temp;
+	}
       }
+
       if(g_KeyA_Pressed){
 	glm::vec4 upw_crossprod = crossproduct(camera_up_vector, camera_w_vector);
-	camera_position_c += -(upw_crossprod / norm(upw_crossprod)) * speed * delta_t;
+	cam_temp += -(upw_crossprod / norm(upw_crossprod)) * speed * delta_t;
+
+	if(!CheckColisionPointWalls(cam_temp)){
+	  camera_position_c = cam_temp;
+	}
       }
 
       glm::mat4 view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
@@ -168,20 +224,98 @@ int main(int argc, char* argv[])
 	* Matrix_Rotate_X(0.2f)
 	* Matrix_Rotate_Y(g_AngleY + (float)glfwGetTime() * 0.1f);
       DrawObject(model, "the_sphere", SPHERE);
+      // DrawObject(model, "cube_faces", bounding_box_object_id);
+
+      BoundingBox obj = {glm::vec4(g_VirtualScene["the_sphere"].bbox_min.x, g_VirtualScene["the_sphere"].bbox_min.y, g_VirtualScene["the_sphere"].bbox_min.z, 1), glm::vec4(g_VirtualScene["the_sphere"].bbox_max.x, g_VirtualScene["the_sphere"].bbox_max.y, g_VirtualScene["the_sphere"].bbox_max.z, 1)};
+
+      // std::cout << "min";
+      // PrintVec4(obj.min);
+      // std::cout << "max";
+      // PrintVec4(obj.max);
+      // std::cout << "size";
+      // PrintVec4(size);
+      // std::cout << "center";
+      // PrintVec4(center);
+
+      // model = model * Matrix_Translate(center.x, center.y, center.z) * Matrix_Scale(size.x, size.y, size.z);
+      // DrawObject(model, "cube", CUBE);
+
 
       // Drawing the portal gun
       model = T_view
-	* Matrix_Translate(1,  -1, -2)
-	* Matrix_Scale(1, 1, 1);
+	* Matrix_Translate(0.2,  -0.1, -0.2)
+	* Matrix_Scale(0.2, 0.2, 0.2);
       DrawObject(model, "PortalGun", PORTALGUN);
+      glm::mat4 modelPortalGun = model;
+
+      obj = {glm::vec4(g_VirtualScene["PortalGun"].bbox_min.x, g_VirtualScene["PortalGun"].bbox_min.y, g_VirtualScene["PortalGun"].bbox_min.z, 1), glm::vec4(g_VirtualScene["PortalGun"].bbox_max.x, g_VirtualScene["PortalGun"].bbox_max.y, g_VirtualScene["PortalGun"].bbox_max.z, 1)};
+
+      model = modelPortalGun * Matrix_Translate(obj.max.x, obj.max.y, obj.max.z) * Matrix_Scale(0.7, 0.7, 0.7);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelPortalGun * Matrix_Translate(obj.max.x, obj.max.y, obj.min.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelPortalGun * Matrix_Translate(obj.max.x, obj.min.y, obj.max.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelPortalGun * Matrix_Translate(obj.max.x, obj.min.y, obj.min.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelPortalGun * Matrix_Translate(obj.min.x, obj.max.y, obj.max.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelPortalGun * Matrix_Translate(obj.min.x, obj.min.y, obj.max.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelPortalGun * Matrix_Translate(obj.min.x, obj.max.y, obj.min.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelPortalGun * Matrix_Translate(obj.min.x, obj.min.y, obj.min.z) * Matrix_Scale(0.3, 0.3, 0.3);
+      DrawObject(model, "cube", CUBE);
+
+
+
+      // model = model * Matrix_Translate(center.x, center.y, center.z) * Matrix_Scale(size.x, size.y, size.z);
+      // DrawObject(model, "cube", CUBE);
 
       // Drawing the bunny
       model = Matrix_Translate(1.0f,0.0f,0.0f)
 	* Matrix_Rotate_X(g_AngleX + (float)glfwGetTime() * 0.1f);
       DrawObject(model, "the_bunny", BUNNY);
+      glm::mat4 modelBunny =  Matrix_Translate(1.0f,0.0f,0.0f) * Matrix_Scale(1.5, 1.5, 1.5);
+      // model = Matrix_Identity();
+      // DrawObject(model, "cube", CUBE);
+
+      obj = {glm::vec4(g_VirtualScene["the_bunny"].bbox_min.x, g_VirtualScene["the_bunny"].bbox_min.y, g_VirtualScene["the_bunny"].bbox_min.z, 1), glm::vec4(g_VirtualScene["the_bunny"].bbox_max.x, g_VirtualScene["the_bunny"].bbox_max.y, g_VirtualScene["the_bunny"].bbox_max.z, 1)};
+
+      model = modelBunny * Matrix_Translate(obj.max.x, obj.max.y, obj.max.z) * Matrix_Scale(0.7, 0.7, 0.7);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelBunny * Matrix_Translate(obj.max.x, obj.max.y, obj.min.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelBunny * Matrix_Translate(obj.max.x, obj.min.y, obj.max.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelBunny * Matrix_Translate(obj.max.x, obj.min.y, obj.min.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelBunny * Matrix_Translate(obj.min.x, obj.max.y, obj.max.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelBunny * Matrix_Translate(obj.min.x, obj.min.y, obj.max.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelBunny * Matrix_Translate(obj.min.x, obj.max.y, obj.min.z) * Matrix_Scale(0.5, 0.5, 0.5);
+      DrawObject(model, "cube", CUBE);
+
+      model = modelBunny * Matrix_Translate(obj.min.x, obj.min.y, obj.min.z) * Matrix_Scale(0.3, 0.3, 0.3);
+      DrawObject(model, "cube", CUBE);
+
 
       // Drawing the planes
-      model = Matrix_Translate(0.0f,-1.1f,0.0f)
+      model = Matrix_Translate(0.0f,-1.1f,0.0f) // chão
 	* Matrix_Scale(10.0f, 1.0f, 10.0f);
       DrawObject(model, "the_plane", PLANE);
 
@@ -209,6 +343,13 @@ int main(int argc, char* argv[])
       DrawObject(model, "the_plane", PLANE);
 
       TextRendering_ShowFramesPerSecond(window);
+      TextRendering_ShowModelViewProjection(
+					    window,
+					    projection,
+					    view,
+					    modelPortalGun,
+					    camera_position_c
+					    );
 
       glfwSwapBuffers(window);
 
@@ -234,5 +375,11 @@ void DrawObject(glm::mat4 model, const char* name, int id){
       glUniformMatrix4fv(g_model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
       glUniform1i(g_object_id_uniform, id);
       DrawVirtualObject(name);
+
+}
+
+void PrintVec4(glm::vec4 vec){
+
+  std::cout << "(" << vec.x << "," << vec.y << "," << vec.z << ")\n";
 
 }
