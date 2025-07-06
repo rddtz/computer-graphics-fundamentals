2fc6c72483ae06af927a1e446c13ecdceb8be425
Rayan Raddatz <rayandematos@gmail.com>
2025-07-06 10:10:08 -0300

    small changes and removed limitation in the dotproduct function

--- include/matrices.h
+++ include/matrices.h
@@ -205,17 +205,17 @@ float dotproduct(glm::vec4 u, glm::vec4 v)
     float u1 = u.x;
     float u2 = u.y;
     float u3 = u.z;
-    float u4 = u.w;
+    /* float u4 = u.w;  */
     float v1 = v.x;
     float v2 = v.y;
     float v3 = v.z;
-    float v4 = v.w;
+    /* float v4 = v.w; */
 
-    if ( u4 != 0.0f || v4 != 0.0f )
-    {
-        fprintf(stderr, "ERROR: Produto escalar não definido para pontos.\n");
-        std::exit(EXIT_FAILURE);
-    }
+    /* if ( u4 != 0.0f || v4 != 0.0f ) */
+    /* { */
+    /*     fprintf(stderr, "ERROR: Produto escalar não definido para pontos.\n"); */
+    /*     std::exit(EXIT_FAILURE); */
+    /* } */
 
     return u1*v1 + u2*v2 + u3*v3;
 }
